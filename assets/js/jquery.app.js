ac81301d68ce81e6d89ce2dd75158bb26b492ee2
Edwin <ed107107@gmail.com>
2022-07-31 11:26:27 +0300

    validated add file

--- assets/js/jquery.app.js
+++ assets/js/jquery.app.js
@@ -6,57 +6,57 @@
 
 
 !function($) {
-    "use strict";
+  "use strict";
 
-    var Sidemenu = function() {
-        this.$body = $("body"),
-        this.$openLeftBtn = $(".open-left"),
-        this.$menuItem = $("#sidebar-menu a")
-    };
-    Sidemenu.prototype.openLeftBar = function() {
-      $("#wrapper").toggleClass("enlarged");
-      $("#wrapper").addClass("forced");
+  var Sidemenu = function() {
+      this.$body = $("body"),
+      this.$openLeftBtn = $(".open-left"),
+      this.$menuItem = $("#sidebar-menu a")
+  };
+  Sidemenu.prototype.openLeftBar = function() {
+    $("#wrapper").toggleClass("enlarged");
+    $("#wrapper").addClass("forced");
 
-      if($("#wrapper").hasClass("enlarged") && $("body").hasClass("fixed-left")) {
-        $("body").removeClass("fixed-left").addClass("fixed-left-void");
-      } else if(!$("#wrapper").hasClass("enlarged") && $("body").hasClass("fixed-left-void")) {
-        $("body").removeClass("fixed-left-void").addClass("fixed-left");
+    if($("#wrapper").hasClass("enlarged") && $("body").hasClass("fixed-left")) {
+      $("body").removeClass("fixed-left").addClass("fixed-left-void");
+    } else if(!$("#wrapper").hasClass("enlarged") && $("body").hasClass("fixed-left-void")) {
+      $("body").removeClass("fixed-left-void").addClass("fixed-left");
+    }
+    
+    if($("#wrapper").hasClass("enlarged")) {
+      $(".left ul").removeAttr("style");
+    } else {
+      $(".subdrop").siblings("ul:first").show();
+    }
+    
+    toggle_slimscroll(".slimscrollleft");
+    $("body").trigger("resize");
+  },
+  //menu item click
+  Sidemenu.prototype.menuItemClick = function(e) {
+     if(!$("#wrapper").hasClass("enlarged")){
+      if($(this).parent().hasClass("has_sub")) {
+
+      }   
+      if(!$(this).hasClass("subdrop")) {
+        // hide any open menus and remove all other classes
+        $("ul",$(this).parents("ul:first")).slideUp(350);
+        $("a",$(this).parents("ul:first")).removeClass("subdrop");
+        $("#sidebar-menu .pull-right i").removeClass("md-remove").addClass("md-add");
+        
+        // open our new menu and add the open class
+        $(this).next("ul").slideDown(350);
+        $(this).addClass("subdrop");
+        $(".pull-right i",$(this).parents(".has_sub:last")).removeClass("md-add").addClass("md-remove");
+        $(".pull-right i",$(this).siblings("ul")).removeClass("md-remove").addClass("md-add");
+      }else if($(this).hasClass("subdrop")) {
+        $(this).removeClass("subdrop");
+        $(this).next("ul").slideUp(350);
+        $(".pull-right i",$(this).parent()).removeClass("md-remove").addClass("md-add");
       }
-      
-      if($("#wrapper").hasClass("enlarged")) {
-        $(".left ul").removeAttr("style");
-      } else {
-        $(".subdrop").siblings("ul:first").show();
-      }
-      
-      toggle_slimscroll(".slimscrollleft");
-      $("body").trigger("resize");
-    },
-    //menu item click
-    Sidemenu.prototype.menuItemClick = function(e) {
-       if(!$("#wrapper").hasClass("enlarged")){
-        if($(this).parent().hasClass("has_sub")) {
-
-        }   
-        if(!$(this).hasClass("subdrop")) {
-          // hide any open menus and remove all other classes
-          $("ul",$(this).parents("ul:first")).slideUp(350);
-          $("a",$(this).parents("ul:first")).removeClass("subdrop");
-          $("#sidebar-menu .pull-right i").removeClass("md-remove").addClass("md-add");
-          
-          // open our new menu and add the open class
-          $(this).next("ul").slideDown(350);
-          $(this).addClass("subdrop");
-          $(".pull-right i",$(this).parents(".has_sub:last")).removeClass("md-add").addClass("md-remove");
-          $(".pull-right i",$(this).siblings("ul")).removeClass("md-remove").addClass("md-add");
-        }else if($(this).hasClass("subdrop")) {
-          $(this).removeClass("subdrop");
-          $(this).next("ul").slideUp(350);
-          $(".pull-right i",$(this).parent()).removeClass("md-remove").addClass("md-add");
-        }
-      } 
-    },
-
+    } 
+  },
+  
     //init sidemenu
     Sidemenu.prototype.init = function() {
       var $this  = this;
@@ -78,55 +78,59 @@
       // NAVIGATION HIGHLIGHT & OPEN PARENT
       $("#sidebar-menu ul li.has_sub a.active").parents("li:last").children("a:first").addClass("active").trigger("click");
     },
-
-    //init Sidemenu
-    $.Sidemenu = new Sidemenu, $.Sidemenu.Constructor = Sidemenu
+        //init Sidemenu
+        $.Sidemenu = new Sidemenu, $.Sidemenu.Constructor = Sidemenu
     
-}(window.jQuery),
+      }(window.jQuery),
+      
+      
+      function($) {
+          "use strict";
+      
+          var FullScreen = function() {
+              this.$body = $("body"),
+              this.$fullscreenBtn = $("#btn-fullscreen")
+          };
+      
+          //turn on full screen
+          // Thanks to http://davidwalsh.name/fullscreen
+          FullScreen.prototype.launchFullscreen  = function(element) {
+            if(element.requestFullscreen) {
+              element.requestFullscreen();
+            } else if(element.mozRequestFullScreen) {
+              element.mozRequestFullScreen();
+            } else if(element.webkitRequestFullscreen) {
+              element.webkitRequestFullscreen();
+            } else if(element.msRequestFullscreen) {
+              element.msRequestFullscreen();
+            }
+          },
+          FullScreen.prototype.exitFullscreen = function() {
+            if(document.exitFullscreen) {
+              document.exitFullscreen();
+            } else if(document.mozCancelFullScreen) {
+              document.mozCancelFullScreen();
+            } else if(document.webkitExitFullscreen) {
+              document.webkitExitFullscreen();
+            }
+          },
+          //toggle screen
+          FullScreen.prototype.toggle_fullscreen  = function() {
+            var $this = this;
+            var fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.webkitFullscreenEnabled;
+            if(fullscreenEnabled) {
+              if(!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+                $this.launchFullscreen(document.documentElement);
+              } else{
+                $this.exitFullscreen();
+              }
+            }
+          },
+          
 
 
-function($) {
-    "use strict";
 
-    var FullScreen = function() {
-        this.$body = $("body"),
-        this.$fullscreenBtn = $("#btn-fullscreen")
-    };
-
-    //turn on full screen
-    // Thanks to http://davidwalsh.name/fullscreen
-    FullScreen.prototype.launchFullscreen  = function(element) {
-      if(element.requestFullscreen) {
-        element.requestFullscreen();
-      } else if(element.mozRequestFullScreen) {
-        element.mozRequestFullScreen();
-      } else if(element.webkitRequestFullscreen) {
-        element.webkitRequestFullscreen();
-      } else if(element.msRequestFullscreen) {
-        element.msRequestFullscreen();
-      }
-    },
-    FullScreen.prototype.exitFullscreen = function() {
-      if(document.exitFullscreen) {
-        document.exitFullscreen();
-      } else if(document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-      } else if(document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-      }
-    },
-    //toggle screen
-    FullScreen.prototype.toggle_fullscreen  = function() {
-      var $this = this;
-      var fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.webkitFullscreenEnabled;
-      if(fullscreenEnabled) {
-        if(!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
-          $this.launchFullscreen(document.documentElement);
-        } else{
-          $this.exitFullscreen();
-        }
-      }
-    },
+	
     //init sidemenu
     FullScreen.prototype.init = function() {
       var $this  = this;
@@ -176,29 +180,28 @@ function($) {
 
       
     },
-    //initilizing 
-    App.prototype.init = function() {
-        var $this = this;
-        //document load initialization
-        $(document).ready($this.onDocReady);
-        //init side bar - left
-        $.Sidemenu.init();
-        //init fullscreen
-        $.FullScreen.init();
-    },
+        //initilizing 
+        App.prototype.init = function() {
+          var $this = this;
+          //document load initialization
+          $(document).ready($this.onDocReady);
+          //init side bar - left
+          $.Sidemenu.init();
+          //init fullscreen
+          $.FullScreen.init();
+      },
+  
+      $.App = new App, $.App.Constructor = App
+  
+  }(window.jQuery),
+  
+  //initializing main application module
+  function($) {
+      "use strict";
+      $.App.init();
+  }(window.jQuery);
 
-    $.App = new App, $.App.Constructor = App
-
-}(window.jQuery),
-
-//initializing main application module
-function($) {
-    "use strict";
-    $.App.init();
-}(window.jQuery);
-
-
-
+  
 /* ------------ some utility functions ----------------------- */
 //this full screen
 var toggle_fullscreen = function () {
@@ -244,7 +247,7 @@ var changeptype = function(){
   toggle_slimscroll(".slimscrollleft");
 }
 
-
+/*
 var debounce = function(func, wait, immediate) {
   var timeout, result;
   return function() {
@@ -260,6 +263,7 @@ var debounce = function(func, wait, immediate) {
     return result;
   };
 }
+*/
 
 function resizeitems(){
   if($.isArray(resizefunc)){  
@@ -270,52 +274,54 @@ function resizeitems(){
 }
 
 function initscrolls(){
-    if(jQuery.browser.mobile !== true){
-      //SLIM SCROLL
-      $('.slimscroller').slimscroll({
-        height: 'auto',
-        size: "5px"
-      });
+  if(jQuery.browser.mobile !== true){
+    //SLIM SCROLL
+    $('.slimscroller').slimscroll({
+      height: 'auto',
+      size: "5px"
+    });
 
-      $('.slimscrollleft').slimScroll({
-          height: 'auto',
-          position: 'right',
-          size: "5px",
-          color: '#98a6ad',
-          wheelStep: 5
-      });
-  }
+    $('.slimscrollleft').slimScroll({
+        height: 'auto',
+        position: 'right',
+        size: "5px",
+        color: '#98a6ad',
+        wheelStep: 5
+    });
+}
 }
 function toggle_slimscroll(item){
-    if($("#wrapper").hasClass("enlarged")){
-      $(item).css("overflow","inherit").parent().css("overflow","inherit");
-      $(item). siblings(".slimScrollBar").css("visibility","hidden");
-    }else{
-      $(item).css("overflow","hidden").parent().css("overflow","hidden");
-      $(item). siblings(".slimScrollBar").css("visibility","visible");
-    }
+  if($("#wrapper").hasClass("enlarged")){
+    $(item).css("overflow","inherit").parent().css("overflow","inherit");
+    $(item). siblings(".slimScrollBar").css("visibility","hidden");
+  }else{
+    $(item).css("overflow","hidden").parent().css("overflow","hidden");
+    $(item). siblings(".slimScrollBar").css("visibility","visible");
+  }
 }
 
 // === following js will activate the menu in left side bar based on url ====
 $(document).ready(function() {
-    $("#sidebar-menu a").each(function() {
-        if (this.href == window.location.href) {
-            $(this).addClass("active");
-            $(this).parent().addClass("active"); // add active to li of the current link
-            $(this).parent().parent().prev().addClass("active"); // add active class to an anchor
-            $(this).parent().parent().parent().parent().prev().click();
-            $(this).parent().parent().prev().click(); // click the item to make it drop
-        }
-    });
+  $("#sidebar-menu a").each(function() {
+      if (this.href == window.location.href) {
+          $(this).addClass("active");
+          $(this).parent().addClass("active"); // add active to li of the current link
+          $(this).parent().parent().prev().addClass("active"); // add active class to an anchor
+          $(this).parent().parent().parent().parent().prev().click();
+          $(this).parent().parent().prev().click(); // click the item to make it drop
+      }
+  });
 });
 
 var wow = new WOW(
-  {
-    boxClass: 'wow', // animated element css class (default is wow)
-    animateClass: 'animated', // animation css class (default is animated)
-    offset: 50, // distance to the element when triggering the animation (default is 0)
-    mobile: false        // trigger animations on mobile devices (true is default)
-  }
+{
+  boxClass: 'wow', // animated element css class (default is wow)
+  animateClass: 'animated', // animation css class (default is animated)
+  offset: 50, // distance to the element when triggering the animation (default is 0)
+  mobile: false        // trigger animations on mobile devices (true is default)
+}
 );
 wow.init();
 
+
+  
