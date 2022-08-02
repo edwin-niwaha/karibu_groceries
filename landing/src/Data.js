f3c5f7f1bf34ba3a6bdcd9d39040e6b1be69761e
Edwin <ed107107@gmail.com>
2022-08-02 23:07:11 +0300

    cart

---
+++ landing/src/Data.js
@@ -0,0 +1,37 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Ground Nuts",
+    price: 4500,
+    desc: "It is also known as the peanut, goober, pindar or monkey nut, It is a legume crop grown mainly for its edible seeds.",
+    img: "./images/nuts1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Cow Peas",
+    price: 2000,
+    desc: "Due to its tolerance for sandy soil and low rainfall, it is an important crop in the semiarid regions across Africa and Asia.",
+    img: "./images/pease2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Soybeans",
+    price: 2500,
+    desc: "It is also called soya bean and is widely grown for its edible bean, which has numerous uses.",
+    img: "./images/soy_slide3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Grain Maize",
+    price: 3000,
+    desc: "This prefers deep and well supplied soils. Well aired soils are beneficial for quick soil warming and good root growth.",
+    img: "./images/grain4.jpg",
+  },
+  {
+    id: "thyfhcbcvx",
+    name: "Grain Maize",
+    price: 6000,
+    desc: "This prefers deep and well supplied soils. Well aired soils are beneficial for quick soil warming and good root growth.",
+    img: "./images/beans1.jpg",
+  },
+];
